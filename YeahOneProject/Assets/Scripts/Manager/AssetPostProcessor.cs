7c9117304b612f67e1c608d2d5fe08c076d5b411
Truong Nguyen <nhattruong159@gmail.com>
2019-04-20 17:38:55 +0700

    Init data: Images, Sprite Atlas, Scripts

---
+++ YeahOneProject/Assets/Scripts/Manager/AssetPostProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetPostProcessor : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+        Debug.Log("<color=green>OnPreprocessTexture </color>" + assetPath);
+        if(assetPath.Contains("BG_"))
+        {
+            TextureImporter _textureImporter = (TextureImporter)assetImporter;
+            _textureImporter.maxTextureSize = 2048;
+        }
+    }
+
+
+}
